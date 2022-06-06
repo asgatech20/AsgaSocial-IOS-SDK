72176824c4f403a674474de2e61f4ffd58d99ed6
Zeynab <zinab.ahmed@asgatech.com>
2022-06-06 16:55:41 +0200

    Initial Commit

---
+++ AsgaSocialSDK/AsgaSocialSDK.h
@@ -0,0 +1,18 @@
+//
+//  AsgaSocialSDK.h
+//  AsgaSocialSDK
+//
+//  Created by Zinab Ahmed on 6/6/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AsgaSocialSDK.
+FOUNDATION_EXPORT double AsgaSocialSDKVersionNumber;
+
+//! Project version string for AsgaSocialSDK.
+FOUNDATION_EXPORT const unsigned char AsgaSocialSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsgaSocialSDK/PublicHeader.h>
+
+
